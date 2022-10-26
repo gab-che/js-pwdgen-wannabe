845733bcc9a46710d92f0a02fc7dada8e8414675
gab-che <gabriellachessa1@gmail.com>
2022-10-26 16:32:28 +0200

    added ;

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 const userName = prompt("Inserisci il tuo nome:")
 
 //chiedo all'utente il cognome e lo salvo in una variabile
-const userSurname = prompt("Inserisci il tuo cognome:")
+const userSurname = prompt("Inserisci il tuo cognome:");
 
 //chiedo all'utente il colore preferito e lo salvo in una variabile
-const userColor = prompt("Inserisci il tuo colore preferito:")
+const userColor = prompt("Inserisci il tuo colore preferito:");
 
 // salvo un numero in una variabile
 const number = 22
 
 // prendo l'elemento html che voglio modificare e lo salvo in una variabile
-const password = document.getElementById("password")
+const password = document.getElementById("password");
 
 // unisco i prompt e li inserisco nell'html all'id="password"
-password.innerHTML = userName + userSurname + userColor + number
+password.innerHTML = userName + userSurname + userColor + number;
